58967f14a01753c2c5fd8991e98bb53020df222e
hazecodez <muhammedhashir298@gmail.com>
2023-10-26 21:04:33 +0530

    handled coupon errors

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -15,3 +15,38 @@ $(document).ready(function() {
       }
     });
   });
+
+// remove product image in product edit page
+
+  function updateImagePreview(inputElement, imagePreviewId, alreadyImg, removeButtonId) {
+    var imagePreview = document.getElementById(imagePreviewId);
+    var file = inputElement.files[0];
+    var removeButton = document.getElementById(removeButtonId);
+  
+    if (file) {
+      var reader = new FileReader();
+  
+      reader.onload = function (e) {
+        imagePreview.src = e.target.result;
+        removeButton.style.display = 'block'; // Show the remove button when a file is selected
+      };
+  
+      reader.readAsDataURL(file);
+    } else {
+      // Restore the original image source when no file is selected
+      imagePreview.src = alreadyImg;
+      removeButton.style.display = 'none'; // Hide the remove button when no file is selected
+    }
+  }
+  
+  
+
+  function removeImagePreview(imagePreviewId, alreadyImg, removeButtonId,inputElement) {
+    var imagePreview = document.getElementById(imagePreviewId);
+    var inputElement = document.getElementById(inputElement);
+    // Restore the original image source when the image is removed
+    imagePreview.src = alreadyImg;
+    var removeButton = document.getElementById(removeButtonId);
+    removeButton.style.display = 'none'; // Hide the remove button
+    inputElement.value=""
+  }
