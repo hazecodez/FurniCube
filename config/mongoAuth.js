ed1d0d709112175afb70d92c0777019b044d9380
hazecodez <muhammedhashir298@gmail.com>
2024-07-28 11:23:34 +0530

    Deployment

--- config/mongoAuth.js
+++ config/mongoAuth.js
@@ -1,19 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-let dotenv = require('dotenv')
-dotenv.config()
+let dotenv = require("dotenv");
+dotenv.config();
 
-module.exports={
-
-   mongoDB:()=>{
-
-       mongoose.connect(process.env.Mongo_url,{
-           
-       }).then(()=>{
-           console.log("Database connected");
-       }).catch((err)=>{
-           console.log(err);
-       })
-   }
-
-}
+module.exports = {
+  mongoDB: () => {
+    mongoose
+      .connect(process.env.Mongo_url, {})
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
